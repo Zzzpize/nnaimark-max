4c928773e7e33db0659751668fe0a8c382797914
Zzzpize <zzzpize@yandex.com>
2025-11-15 04:08:06 +0500

    Render connection

--- backend/database.py
+++ backend/database.py
@@ -1,10 +1,13 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from models import Base
 
-DATABASE_URL = "sqlite:///./app.db"
+# Теперь URL будет браться из переменных окружения, что гораздо безопаснее
+# Для локальной разработки можно оставить старый DATABASE_URL = "sqlite:///./app.db"
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./app.db")
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 def create_db_and_tables():
